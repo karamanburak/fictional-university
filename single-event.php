9bf520f7f00ee33c4931920f1d0d8146e377d94c
Burak Karaman <karaman.buraak@gmail.com>
2025-03-22 00:19:26 +0100

    [Feat] Add related programs section to single-event.php and event summary section to single-program.php

--- single-event.php
+++ single-event.php
@@ -23,6 +23,26 @@ while(have_posts()) {
 
         <div class="generic-content"><?php the_content(); ?></div>
 
+        <?php 
+        
+        $relatedPrograms = get_field("related_programs");
+        // print_r($relatedPrograms);
+
+        if ($relatedPrograms) {
+        echo "<hr class='section-break'>";
+        echo " <h2 class='headline headline--medium'>Reletad Program(s)</h2> ";
+        echo "<ul class='link-list min-list'>";
+        }
+
+        forEach($relatedPrograms as $program) { ?>
+        <li><a href="<?php get_the_permalink($program) ?>"><?php echo get_the_title($program) ?> </a> </li>
+         
+       <?php }
+
+       echo "</ul>"
+
+        ?>
+
     </div>
 
     <?php
