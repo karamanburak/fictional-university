d0528ffe493dc52cf7d5136912a3acda4a349cc3
Burak Karaman <karaman.buraak@gmail.com>
2025-03-22 22:22:20 +0100

    [Feat] Add related professors query and display to single-program.php

--- single-program.php
+++ single-program.php
@@ -27,6 +27,33 @@ while(have_posts()) {
     <div class="generic-content"><?php the_content(); ?></div>
 
     <?php 
+
+     $relatedProfessors = new WP_Query(array(
+              "posts_per_page" => -1,
+            //   "posts_per_page" => 2,
+              "post_type" => "professor",
+              "orderby" => "title",
+              "order" => "ASC",
+              "meta_query" => array(
+                array(
+                    "key" => "related_programs",
+                    "compare" => "LIKE",
+                    "value" => "'" . get_the_ID() . "'",
+                )
+              )
+            ));
+
+            if ($relatedProfessors->have_posts()) {
+            echo "<hr class='section-break'>";
+            echo "<h2 class='headline headline--meidum'>Upcoming " . get_the_title() . " Events</h2>";
+
+            while($relatedProfessors -> have_posts()) {
+              $relatedProfessors -> the_post(); ?>
+           <li><a href=""><?php the_permalink(); ?></a> </li>
+
+    <?php }
+            }
+
              $today = date("Ymd");
              $homepageEvents = new WP_Query(array(
               // "posts_per_page" => -1,
