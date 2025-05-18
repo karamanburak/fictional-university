9d533e60e9870303a6e309cff4ad35b340471e41
Burak Karaman <karaman.buraak@gmail.com>
2025-05-18 22:41:25 +0200

    [Feat] Update search functionality to fetch and display results dynamically from API

--- build/index.js
+++ build/index.js
@@ -4153,8 +4153,15 @@ class Search {
     }
   }
   getResults() {
-    this.resultsDiv.html("Imagine real search results here...");
-    this.isSpinnerVisible = false;
+    jquery__WEBPACK_IMPORTED_MODULE_0___default().getJSON("http://fictional-university.local/wp-json/wp/v2/posts?search=" + this.searchField.val(), posts => {
+      this.resultsDiv.html(`
+        <h2 class="search-overlay__section-title">General Information</h2>
+        <ul class="link-list min-list">
+        ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join("")}
+
+        </ul>
+        `);
+    });
   }
   keyPressDispatcher(e) {
     if (e.keyCode == 83 && !this.isOverlayOpen && !jquery__WEBPACK_IMPORTED_MODULE_0___default()("input", textarea).is(":focus")) {
--- src/modules/Search.js
+++ src/modules/Search.js
@@ -45,12 +45,27 @@ class Search {
   }
 
   getResults() {
-    this.resultsDiv.html("Imagine real search results here...");
-    this.isSpinnerVisible = false;
+    $.getJSON(
+      "http://fictional-university.local/wp-json/wp/v2/posts?search=" +
+        this.searchField.val(),
+      (posts) => {
+        this.resultsDiv.html(`
+        <h2 class="search-overlay__section-title">General Information</h2>
+        <ul class="link-list min-list">
+        ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join("")}
+
+        </ul>
+        `);
+      }
+    );
   }
 
   keyPressDispatcher(e) {
-    if (e.keyCode == 83 && !this.isOverlayOpen && !$("input", textarea).is(":focus")) {
+    if (
+      e.keyCode == 83 &&
+      !this.isOverlayOpen &&
+      !$("input", textarea).is(":focus")
+    ) {
       this.openOverlay();
     }
 
