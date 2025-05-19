12caf3e70eef11f15e25bbbf4ac46a06920a55c3
Burak Karaman <karaman.buraak@gmail.com>
2025-05-19 22:33:42 +0200

    [Feat] Integrate author name into search results and enhance REST API with custom fields for posts

--- functions.php
+++ functions.php
@@ -1,7 +1,21 @@
 <?php
 
-function pageBanner($args = [] ) {
-  
+require get_theme_file_path("/inc/search-route.php");
+
+function university_custom_rest()
+{
+  register_rest_field("post", "authorName", [
+    "get_callback" => function () {
+      return get_the_author();
+    }
+  ]);
+}
+
+add_action("rest_api_init", "university_custom_rest");
+
+function pageBanner($args = [])
+{
+
   if (!isset($args['title'])) {
     $args['title'] = get_the_title();
   }
@@ -18,7 +32,7 @@ function pageBanner($args = [] ) {
     }
   }
 
-  ?>
+?>
   <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
     <div class="page-banner__content container container--narrow">
@@ -26,12 +40,13 @@ function pageBanner($args = [] ) {
       <div class="page-banner__intro">
         <p><?php echo $args['subtitle']; ?></p>
       </div>
-    </div>  
+    </div>
   </div>
 <?php }
 
-function university_files() {
- /* wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=YOUR_API_KEY', NULL, '1.0', true); */
+function university_files()
+{
+  /* wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=YOUR_API_KEY', NULL, '1.0', true); */
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
@@ -49,7 +64,8 @@ function university_files() {
 
 add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features() {
+function university_features()
+{
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
   add_image_size('professorLandscape', 400, 260, true);
@@ -59,36 +75,38 @@ function university_features() {
 
 add_action('after_setup_theme', 'university_features');
 
-function university_adjust_queries($query) {
-  if (!is_admin() AND is_post_type_archive('campus') AND $query->is_main_query()) {
+function university_adjust_queries($query)
+{
+  if (!is_admin() and is_post_type_archive('campus') and $query->is_main_query()) {
     $query->set('posts_per_page', -1);
   }
 
-  if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+  if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
     $query->set('orderby', 'title');
     $query->set('order', 'ASC');
     $query->set('posts_per_page', -1);
   }
 
-  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+  if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
     $today = date('Ymd');
     $query->set('meta_key', 'event_date');
     $query->set('orderby', 'meta_value_num');
     $query->set('order', 'ASC');
-    $query->set('meta_query',[
-             [
-                'key' => 'event_date',
-                'compare' => '>=',
-                'value' => $today,
-                'type' => 'numeric'
-              ]
-            ]);
+    $query->set('meta_query', [
+      [
+        'key' => 'event_date',
+        'compare' => '>=',
+        'value' => $today,
+        'type' => 'numeric'
+      ]
+    ]);
   }
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
 
-function universityMapKey($api) {
+function universityMapKey($api)
+{
   $api["key"] = "$APIKey";
   return $api;
 }
---
+++ inc/search-route.php
@@ -0,0 +1,28 @@
+<?php 
+
+add_action("rest_api_init", "universityRegisterSearch");
+
+function universityRegisterSearch () {
+    register_rest_route("university/v1", "search", [
+        "methods" => WP_REST_Server::READABLE,
+        "callback" => "universitySearchResults"
+    ]);
+}
+
+function universitySearchResults () {
+   $professors = new WP_Query([
+    "post_type" => "professor"
+   ]);
+
+   $professorResults = [];
+
+   while ($professors->have_posts()) {
+    $professors->the_post();
+    array_push($professorResults, [
+        "title" => get_the_title(),
+        "permalink" => get_the_permalink()
+    ]);
+   }
+
+   return $professorResults;
+}
