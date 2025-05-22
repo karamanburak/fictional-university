afbb13ba788ed6d4582e2dd935ee4136e8079ffa
Burak Karaman <karaman.buraak@gmail.com>
2025-05-23 00:03:46 +0200

    [Feat] Add "My Notes" link to header for logged-in users, improving navigation

---
+++ page-my-notes.php
@@ -0,0 +1,41 @@
+<?php
+
+if (!is_user_logged_in()) {
+    wp_redirect(esc_url(site_url("/")));
+    exit;
+}
+
+get_header();
+
+while (have_posts()) {
+    the_post();
+    pageBanner();
+?>
+
+    <div class="container container--narrow page-section">
+        <ul class="min-list link-list" id="my-notes">
+            <?php 
+                $userNotes = new WP_Query([
+                    "post_type" => "note",
+                    "posts_per_page" => -1,
+                    "author" => get_current_user_id()
+                ]);
+
+                while ($userNotes->have_posts()) {
+                    $userNotes->the_post(); ?>
+                    <div class="list-group">
+                        <input class="note-title-field" type="text" value="<?= esc_attr(get_the_title()); ?>">
+                        <span class="edit-note"><i clas="fa fa-pencil" aria-hidden="true"></i>Edit</span>
+                        <span class="delete-note"><i clas="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
+                        <textarea class="note-body-field" name="" id=""><?= esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+                    </div>
+                <?php }
+            ?>
+        </ul>
+    </div>
+
+<?php }
+
+get_footer();
+
+?>
