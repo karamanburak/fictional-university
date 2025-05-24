5e76dff63c34dc6967a75216b606ebaeb12d06e5
Burak Karaman <karaman.buraak@gmail.com>
2025-05-24 21:33:30 +0200

    [Feat] Enhance MyNotes functionality with editable fields and AJAX save feature

--- page-my-notes.php
+++ page-my-notes.php
@@ -14,22 +14,23 @@ while (have_posts()) {
 
     <div class="container container--narrow page-section">
         <ul class="min-list link-list" id="my-notes">
-            <?php 
-                $userNotes = new WP_Query([
-                    "post_type" => "note",
-                    "posts_per_page" => -1,
-                    "author" => get_current_user_id()
-                ]);
+            <?php
+            $userNotes = new WP_Query([
+                "post_type" => "note",
+                "posts_per_page" => -1,
+                "author" => get_current_user_id()
+            ]);
 
-                while ($userNotes->have_posts()) {
-                    $userNotes->the_post(); ?>
-                    <div class="list-group">
-                        <input class="note-title-field" type="text" value="<?= esc_attr(get_the_title()); ?>">
-                        <span class="edit-note"><i clas="fa fa-pencil" aria-hidden="true"></i>Edit</span>
-                        <span class="delete-note"><i clas="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
-                        <textarea class="note-body-field" name="" id=""><?= esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
-                    </div>
-                <?php }
+            while ($userNotes->have_posts()) {
+                $userNotes->the_post(); ?>
+                <li data-id="<?php the_ID(); ?>">
+                    <input readonly class="note-title-field" type="text" value="<?= esc_attr(get_the_title()); ?>">
+                    <span class="edit-note"><i clas="fa fa-pencil" aria-hidden="true"></i>Edit</span>
+                    <span class="delete-note"><i clas="fa fa-trash-o" aria-hidden="true"></i>Delete</span>
+                    <textarea readonly class="note-body-field" name="" id=""><?= esc_attr(wp_strip_all_tags(get_the_content())); ?></textarea>
+                    <span class="update-note btn btn--blue btn--small"><i clas="fa fa-arrow-right" aria-hidden="true"></i>Save</span>
+                </li>
+            <?php }
             ?>
         </ul>
     </div>
