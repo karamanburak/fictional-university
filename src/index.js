6b28acf4f605cdf480cbab68160fb59379e810e3
Burak Karaman <karaman.buraak@gmail.com>
2025-05-30 00:04:23 +0200

    [Feat] Add like functionality to professor profiles with AJAX integration for user interactions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import HeroSlider from "./modules/HeroSlider"
 import Search from "./modules/Search"
 import GoogleMap from "./modules/GoogleMap"
 import MyNotes from "./modules/MyNotes"
+import Like from "./modules/Like"
 
 // Instantiate a new object using our modules/classes
 const mobileMenu = new MobileMenu()
@@ -13,4 +14,5 @@ const heroSlider = new HeroSlider()
 const search = new Search()
 const googleMap = new GoogleMap()
 const myNotes = new MyNotes()
+const like = new Like()
 
